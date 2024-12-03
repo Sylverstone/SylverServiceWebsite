5e8776f797fe64a3100e93c9c3ebabce7869373d
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2024-12-03 22:07:43 +0100

    changement script pour utiliser la syntaxe modern EsModule et pas commonJS

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import sendmail from './Scripts/sendMail.js';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use('/Css', express.static(path.join(__dirname, '/Css')));
 app.use('/Scripts', express.static(path.join(__dirname, '/Scripts')));
 app.use('/pages', express.static(path.join(__dirname, '/pages')));
 app.use('/Images', express.static(path.join(__dirname, '/Images')));
-app.use(express.static(path.join(__dirname)));
+
 
 app.get('/', (req, res) => {
-    console.log(req.query());
-    return res.sendFile(path.join(__dirname, '/index.html'));
+    console.log(req.query);
+    return res.sendFile(path.join(__dirname, 'index.html'));
 })
+
+app.use(express.static(path.join(__dirname)));
+
+
 
 app.get('/form', (req, res) => {
     console.log(req.query);
-    return res.send('<script>alert("Votre message a été prit en compte"); window.location.assign("index.html")</script>');
+    if(sendmail(req.query))
+    {
+        return res.send('<script>alert("Votre formulaires a été envoyé"); window.location.assign("index.html")</script>');
+    }
+    return res.send('<script>alert("Une erreur a eu lieu :( ); window.location.assign("index.html")</script>')
 });
 
+app.use((req, res) => {
+    return res.status(404).sendFile(path.join(__dirname, 'pages','404.html'));
+})
 
 app.listen(80, () => {
     console.log('Server running on port 80');
