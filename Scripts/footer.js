68c4fa81c8d235766f2274aeb64d99b75201cd26
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2024-12-03 22:09:35 +0100

    Ajout d'un script sendmail a part | mit en place du footer en script, pour l'avoir sur plusieurs pages | mit en place du texte de mentionLegal.html en .js par soucis de beauté html

---
+++ Scripts/footer.js
@@ -0,0 +1,21 @@
+const footer = document.querySelector('footer');
+
+footer.innerHTML = 
+`
+<div>
+    <a href="pages/mentionLegal.html" target="_blank">Mention légales</a>
+    <img src="Images/justice.png" alt="mention légales" class="footerIMG">
+</div>
+<div>
+    <a href="">Mon Github</a>
+    <img src="Images/github.svg" alt="mention légales" class="footerIMG">
+</div>
+<div>
+    <a href="">Mon LinkedIn</a>
+    <img src="Images/Icon_linkedin.png" alt="mention légales" class="footerIMG">
+</div>  
+<div>
+    <a href="mailto:SylverService@outlook.fr">Me contacter</a>
+    <img src="Images/icon_email.png" alt="mention légales" class="footerIMG">
+</div>
+`
---
+++ Scripts/sendMail.js
@@ -0,0 +1,44 @@
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+import nodemailer from 'nodemailer';
+
+export default function sendmail(dataForm) {
+    console.log('Envoyer depuis :', process.env.EMAILOUT);
+    
+    const transporteur = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: process.env.EMAILIN,
+            pass: process.env.PASSWORD,
+        },
+    });
+        
+    const html = `<h1>Email Automatique :)</h1>
+                  <ul>
+                      <li>Nom : ${dataForm.Nom}</li>
+                      <li>Prénom : ${dataForm.Prenom}</li>
+                      <li>Email : ${dataForm.email}</li>
+                      <li>Type d'individu : ${dataForm.TypeEntreprise}</li>
+                      <li>Message : ${dataForm.textZone}</li>
+                  </ul>`;
+
+    const mailOptions = {
+        from: process.env.EMAILIN,
+        to: process.env.EMAILOUT,
+        subject: "Formulaire Sylver Service",
+        html: html,
+    };
+
+    let bool = true;
+    transporteur.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Erreur lors de l\'envoi :', error);
+            bool = false;
+        } 
+        console.log('E-mail envoyé :', info.response);
+    });
+    return bool;
+}
