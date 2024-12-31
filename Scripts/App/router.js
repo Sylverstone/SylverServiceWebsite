4d7568abb7528b1f18aaebd443b8f68e84fe9842
Sylverstone <sylvio0801@gmail.com>
2024-12-31 01:35:47 +0100

    séparation du fichier app.js pour une meilleur maintenabilité | les fonctions sont donc dans router.js | ajout du fichier dirname.js qui servira uniquement a prendre le chemin jusqu'au dossier tant que je trouve pas autre chose

---
+++ Scripts/App/router.js
@@ -0,0 +1,118 @@
+import { texts } from '../Site/text.js';
+import {footer, header, pageToTextId, pages} from './pagesHtml.js';
+import fetch from 'node-fetch';
+import express from 'express';
+import fs from 'fs';
+import __dirname from '../../dirname.js';
+import path from 'path'
+
+const completePage = (app,url) =>
+{
+    
+    app.get(url, async (req, res) => {
+        const dataPage = pages[url]
+        const Origintemplate = dataPage["template"];
+        let template;
+        if(dataPage["isComplete"] === false)
+        {
+            console.log("charging")
+            template = Origintemplate.replace('{{header}}', header).replace('{{footer}}',footer);
+            const data = pageToTextId[url];
+            if(data !== undefined)
+            {
+                const nbText = data["nombreText"];
+                const idText = data["idText"];
+                for(let i = 1; i < nbText + 1; i++)
+                {
+                    template = template.replace('{{texte'+i+'}}', texts[idText][i-1]);
+                }
+            }       
+            if(url == "/" || url == "/index.html")
+            {
+                const response = await fetch("https://sylverservice.up.railway.app/getImage");
+                const images = await response.json();
+                let Imageshtml = "";
+                Imageshtml += "<aside id=\"ImagesSylverservice\">";
+                const BaseElementImage = "<img src=\"{{src}}\" alt=\"Images de l'application\">";
+                let elementImage;
+                images.forEach(image => {
+                    elementImage = BaseElementImage.replace("{{src}}",image);
+                    Imageshtml += "\n" + elementImage;
+                })
+                Imageshtml += "\n</aside>"
+                template = template.replace("{{images}}", Imageshtml)
+                pages['/'] = {"template" : template, "isComplete" : true}
+                pages['/index.html'] = {"template" : template, "isComplete" : true};
+               
+            }
+            else
+            {
+                pages[url] = {"template" : template, "isComplete" : true};
+            }
+            
+        }
+
+        else
+        {
+            template = Origintemplate;
+            console.log("Already charged")
+        }
+        return res.send(template);
+
+    });
+}
+export const setupAppUse = (app) => 
+{
+    app.use('/Css', express.static(path.join(__dirname, 'Css')));
+    app.use('/Images', express.static(path.join(__dirname, 'Images')));
+    app.use('/Scripts/App', express.static(path.join(__dirname, 'Scripts','App')));
+    app.use('/Scripts/Site', express.static(path.join(__dirname, 'Scripts','Site')));
+    console.log(path.join(__dirname,'Scripts'))
+}
+
+export const getImageRoute = (app) =>
+{
+    app.get('/getImage', (req, res) => {
+
+        console.log("in there")
+        fs.readdir(path.join("Images","Sylverservice"), (err,files) =>
+        {
+            if(err)
+            {
+                console.error('Erreur lors de la lecture des images',err);
+                return;
+            }
+
+            files.forEach( (file,i) => {
+                files[i] = path.join("Images","Sylverservice",file);
+            })
+            res.json(files);
+        });
+    });
+}
+    
+export const formRoute = (app) =>
+{
+    app.get('/form', (req, res) => {
+        console.log(req.query);
+        if(sendmail(req.query))
+        {
+            return res.send('<script>alert("Votre formulaires a été envoyé"); window.location.assign("index.html")</script>');
+        }
+        return res.send('<script>alert("Une erreur a eu lieu :( ); window.location.assign("index.html")</script>')
+    });
+}
+
+export const handle404 = app => 
+{
+    app.use((req, res) => {
+        return res.status(404).sendFile(path.join(__dirname, '404','404.html'));
+    })
+}
+
+export const setupSitePageAvailable = app =>
+{
+    Object.keys(pages).forEach(url => 
+        completePage(app,url)
+    );
+}
--- app.js
+++ app.js
@@ -1,76 +1,22 @@
 import express from 'express';
-import path from 'path';
-import sendmail from './Scripts/sendMail.js';
-import fs from 'fs';
-import { texts } from './Scripts/text.js';
-import {footer, header, pageToTextId, pages, __dirname} from './Scripts/variables.js';
+import {formRoute,getImageRoute,handle404,setupAppUse, setupSitePageAvailable } from './Scripts/App/router.js';
+import path from 'path'
+
+import __dirname from './dirname.js';
 
 const app = express();
 
+setupAppUse(app);
 
-app.use('/Css', express.static(path.join(__dirname, '/Css')));
-app.use('/Scripts', express.static(path.join(__dirname, '/Scripts')));
-app.use('/Images', express.static(path.join(__dirname, '/Images')));
 
-const completePage = (url) =>
-{
-    let template = pages[url];
-    app.get(url,(req, res) => {
-        console.log("user is requesting",url);
-        template = template.replace('{{header}}', header).replace('{{footer}}',footer);
-        const data = pageToTextId[url];
-        console.log(data);
-        if(data !== undefined)
-        {
-            const nbText = data[1];
-            const idText = data[0];
-            for(let i = 1; i < nbText + 1; i++)
-            {
-                console.log('{{texte'+i+'}}');
-                template = template.replace('{{texte'+i+'}}', texts[idText][i-1]);
-            }
-        }       
-        return res.send(template);
-    });
 
-}
+setupSitePageAvailable(app);
 
-Object.keys(pages).forEach(url => 
-    completePage(url)
-);
+getImageRoute(app);
 
-app.use('/pages', express.static(path.join(__dirname, '/pages')));
+formRoute(app);
 
-app.get('/getImage', (req, res) => {
-
-    fs.readdir(path.join("Images","Sylverservice"), (err,files) =>
-    {
-        if(err)
-        {
-            console.error('Erreur lors de la lecture des images',err);
-            return;
-        }
-
-        files.forEach( (file,i) => {
-            files[i] = path.join("Images","Sylverservice",file);
-        })
-        res.json(files);
-    });
-
-});
-
-app.get('/form', (req, res) => {
-    console.log(req.query);
-    if(sendmail(req.query))
-    {
-        return res.send('<script>alert("Votre formulaires a été envoyé"); window.location.assign("index.html")</script>');
-    }
-    return res.send('<script>alert("Une erreur a eu lieu :( ); window.location.assign("index.html")</script>')
-});
-
-app.use((req, res) => {
-    return res.status(404).sendFile(path.join(__dirname, '404','404.html'));
-})
+handle404(app);
 
 app.listen(80, () => {
     console.log('Server running on port 80');
