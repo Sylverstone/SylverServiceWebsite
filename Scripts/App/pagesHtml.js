31f4fcc9ed177857ef6bbf179b2470e19a8de843
Sylverstone <sylvio0801@gmail.com>
2025-01-20 19:43:07 +0100

    utilisation de tinyPng pour les images + suppression de la redirection permanente

--- Scripts/App/pagesHtml.js
+++ Scripts/App/pagesHtml.js
@@ -16,7 +16,7 @@ const legalMention = loadTemplate(path.join("pages","mentionLegal.html"));
 let pages =
 {
     '/' : {"template" : index, "isComplete" : false},
-    '/index.html' : {"template" : index, "isComplete" : false},
+    '/index.html' : {"template" : index, "isComplete" : false}, //a retirer mais on sait jamais
     '/pages/form.html' : {"template" : form, "isComplete" : false},
     '/pages/Techniques.html' : {"template" : Technique, "isComplete" : false},
     '/pages/mentionLegal.html' : {"template" : legalMention, "isComplete" : false}
@@ -25,7 +25,7 @@ let pages =
 const pageToTextId = 
 {
     "/" : {"idText" : 0, "nombreText" : 3},
-    "/index.html" : {"idText" : 0, "nombreText" : 3},
+    "/index.html" : {"idText" : 0, "nombreText" : 3}, //a retirer mais on sait jamais
     "/pages/Techniques.html" : {"idText" : 1, "nombreText" : 1},
     "/pages/mentionLegal.html" : {"idText" : 2, "nombreText" : 1}
     
@@ -35,7 +35,7 @@ const header =
 `
 <nav class="fixe-nav">
     <ul class="nav-ul">
-        <li><a class="lien-nav" href="/index.html">Sylver Service</a></li>
+        <li><a class="lien-nav" href="/">Sylver Service</a></li>
         <li><a class="lien-nav" href="/pages/Techniques.html">Techniques</a></li>
         <li><a class="lien-nav" href="/pages/form.html">Formulaires</a></li>
     </ul>
