bbdff574fe08df6ee89df447721ab90f4459d7f7
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2025-01-31 10:26:48 +0100

    changement api

--- admin/indexNow.js
+++ admin/indexNow.js
@@ -2,25 +2,37 @@ import axios from "axios";
 import fs from 'fs'
 import __dirname from "../dirname.js";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const zoneRecherche = [__dirname,path.join(__dirname,"pages")]
 const relatifZone = ["/", "/pages"]
 const pageRestreinte = ["googleec111dafdd73fd90.html",]
+
 function indexNow()
 {
     let urlList = [];
     zoneRecherche.forEach((dir,i) => {
         let files = fs.readdirSync(dir);
         files = files.filter((file) => path.extname(file) == ".html" && !pageRestreinte.includes(file) )
-        const filteredFile = files.map(value => new URL(path.join(relatifZone[i], value), "https://sylverservice.up.railway.app").toString())
+        const filteredFile = files.map(value => {
+            if(i === 0 && value === "index.html")
+            {
+                value = "/";
+            }
+            return new URL(path.join(relatifZone[i], value), "https://sylverservice.up.railway.app").toString()
+
+        })
+            
         urlList.push(...filteredFile)
     })
 
     console.log(urlList);
     const data = {
         host: "sylverservice.up.railway.app",
-        key: "7ce1b11f48bf459286f3d28f4bfc0099",
-        keyLocation: "https://sylverservice.up.railway.app/7ce1b11f48bf459286f3d28f4bfc0099.txt",
+        key: process.env.indexNowKey,
+        keyLocation: `https://sylverservice.up.railway.app/${process.env.indexNowKey}.txt`,
         urlList: urlList
       };
     
@@ -31,7 +43,6 @@ function indexNow()
     })
     .then(response => console.log(response))
     .catch(error => console.log(error));
-   
 }
 
 indexNow();
